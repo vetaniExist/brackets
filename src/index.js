9dbf59699907935cea30aa4e4d77c1a98e165d49
Huriosity <vetani3123@mail.ru>
2020-10-06 15:09:48 +0300

    feat: complete brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+
+  let openBracketsCounter = 0;
+  let closeBracketsCounter = 0;
+  let aBrackets = [];
+
+  for (let i = 0; i < str.length; i += 1) {
+    for( let j = 0; j < bracketsConfig.length; j += 1) {
+      if (str[i] === bracketsConfig[j][0]) {
+        if (bracketsConfig[j][0] === bracketsConfig[j][1]) {
+          if (openBracketsCounter === closeBracketsCounter) {
+            aBrackets.push(str[i]);
+            openBracketsCounter += 1;
+          } else if (aBrackets[aBrackets.length - 1] === bracketsConfig[j][0]){
+            aBrackets.pop();
+            closeBracketsCounter += 1;
+          }
+          else {
+            aBrackets.push(str[i]);
+            openBracketsCounter += 1;
+          }
+
+        } else {
+          aBrackets.push(str[i]);
+          openBracketsCounter += 1;
+        }
+      } else if (str[i] === bracketsConfig[j][1]) {
+        if (aBrackets[aBrackets.length - 1] === bracketsConfig[j][0]) {
+          aBrackets.pop();
+          closeBracketsCounter += 1;
+        } 
+      }
+    }
+  }
+  return aBrackets.length === 0;
 }
